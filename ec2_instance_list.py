f1f95b4587171db84c675e849dc72826776a7c5e
Thinh Pham <tpham12345@yahoo.com>
2016-09-20 11:21:12 -0500

    Change how region name are retrieved from command line parameter

--- ec2_instance_list.py
+++ ec2_instance_list.py
@@ -2,22 +2,19 @@
 import sys
 import boto3
 
-ec2 = None
-if len(sys.argv) <= 1:
-    ec2 = boto3.resource('ec2')
-else:
-    session = boto3.Session(profile_name=sys.argv[1])
-    ec2 = session.resource('ec2')
+ec2 = boto3.resource('ec2')
+if len(sys.argv) > 1:
+    ec2 = boto3.Session(profile_name=sys.argv[1]).resource('ec2')
 
-for i in ec2.instances.all():
+for item in ec2.instances.all():
     tag_name = 'UNKNOWN'
-    if i.tags is not None:
-        for tag in i.tags:
+    if item.tags is not None:
+        for tag in item.tags:
             if tag['Key'] == 'Name':
                 tag_name = tag['Value']
 
     state_name = 'UNKNOWN'
-    if i.state is not None:
-        state_name = i.state['Name']
+    if item.state is not None:
+        state_name = item.state['Name']
 
-    print 'id: %s, state: %s, name: %s' % (i.id, state_name, tag_name)
+    print 'id: %s, state: %s, name: %s' % (item.id, state_name, tag_name)
--- s3_bucket_list.py
+++ s3_bucket_list.py
@@ -2,12 +2,9 @@
 import sys
 import boto3
 
-s3 = None
-if len(sys.argv) <= 1:
-    s3 = boto3.resource('s3')
-else:
-    session = boto3.Session(profile_name=sys.argv[1])
-    s3 = session.resource('s3')
+s3 = boto3.resource('s3')
+if len(sys.argv) > 1:
+    s3 = boto3.Session(profile_name=sys.argv[1]).resource('s3')
 
-for i in s3.buckets.all():
-    print('name: %s' % i.name)
+for item in s3.buckets.all():
+    print('name: %s' % item.name)
