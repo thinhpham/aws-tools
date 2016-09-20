f350e1dade6eff0aa82312dc9f66e98da19351eb
Thinh Pham <tpham12345@yahoo.com>
2016-09-20 09:21:32 -0500

    Fix a unicode bug when printing EC2 instance types on Windows machines

--- ec2_instance_types_list.py
+++ ec2_instance_types_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
 import urllib
+import sys
+import codecs
 
+# Change encoding to fix a bug on windows
+sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')
+
+# Download a copy of the EC2 Instance Types from Amazon and scrape the content
 site_content = urllib.urlopen('https://aws.amazon.com/ec2/instance-types/').read()
 soup = BeautifulSoup(site_content, 'lxml')
 aws_tables = soup.find_all("div", {"class": "aws-table"});
