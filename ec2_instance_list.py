79d153d384cce1056e08f68e0492e7cfcac87615
Thinh Pham <tpham12345@yahoo.com>
2016-09-28 13:37:20 -0500

    Start using click for command line options

--- ec2_instance_list.py
+++ ec2_instance_list.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import sys
 import boto3
+import click
 
-ec2 = boto3.resource('ec2')
-if len(sys.argv) > 1:
-    ec2 = boto3.Session(profile_name=sys.argv[1]).resource('ec2')
+@click.command()
+@click.option('-p', '--profile', default='default', help='Profile name to use.')
+def cli(profile):
+    ec2 = boto3.Session(profile_name=profile).resource('ec2')
 
-for item in ec2.instances.all():
-    tag_name = 'UNKNOWN'
-    if item.tags is not None:
-        for tag in item.tags:
-            if tag['Key'] == 'Name':
-                tag_name = tag['Value']
+    for item in ec2.instances.all():
+        tag_name = 'UNKNOWN'
+        if item.tags is not None:
+            for tag in item.tags:
+                if tag['Key'] == 'Name':
+                    tag_name = tag['Value']
 
-    state_name = 'UNKNOWN'
-    if item.state is not None:
-        state_name = item.state['Name']
+        state_name = 'UNKNOWN'
+        if item.state is not None:
+            state_name = item.state['Name']
 
-    print 'id: %s, state: %s, name: %s' % (item.id, state_name, tag_name)
+        print 'id: %s, state: %s, name: %s' % (item.id, state_name, tag_name)
+
+if __name__ == '__main__':
+    cli()
--- ec2_instance_status.py
+++ ec2_instance_status.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python
 import sys
+import click
 from aws_util import Ec2Util
 
-if len(sys.argv) == 1:
-    print('Usage: %s (instance_id)' % sys.argv[0])
-    sys.exit(1)
+@click.command()
+@click.option('-p', '--profile', default='default', help='Profile name to use.')
+@click.argument('id_or_tag')
+def cli(profile, id_or_tag):
+    if id_or_tag is not None:
+        ec2 = Ec2Util(profile)
+        instance = ec2.get_instance(id_or_tag)
 
-id_or_tag = sys.argv[1]
-ec2 = Ec2Util()
-instance = ec2.get_instance(id_or_tag)
+        if instance:
+            tag_name = 'UNKNOWN'
+            if instance.tags is not None:
+                for tag in instance.tags:
+                    if tag['Key'] == 'Name':
+                        tag_name = tag['Value']
 
-tag_name = 'UNKNOWN'
-if instance.tags is not None:
-    for tag in instance.tags:
-        if tag['Key'] == 'Name':
-            tag_name = tag['Value']
+            print('id:                  %s' % instance.id)
+            print('name:                %s' % tag_name)
+            print('image_id:            %s' % instance.image_id)
+            print('instance_id:         %s' % instance.instance_id)
+            print('instance_type:       %s' % instance.instance_type)
+            print('platform:            %s' % instance.platform)
+            print('private_dns_name:    %s' % instance.private_dns_name)
+            print('private_ip_address:  %s' % instance.private_ip_address)
+            print('public_dns_name:     %s' % instance.public_dns_name)
+            print('public_ip_address:   %s' % instance.public_ip_address)
+            print('security_groups:     %s' % instance.security_groups)
+            print('state:               %s' % instance.state)
+            print('state_reason:        %s' % instance.state_reason)
+            print('vpc:                 %s' % instance.vpc)
+            print('subnet:              %s' % instance.subnet)
+        else:
+            print('Cannot find instance. Did you select a correct profile?')
+    else:
+        sys.exit(1)
 
-print('id:                  %s' % instance.id)
-print('name:                %s' % tag_name)
-print('image_id:            %s' % instance.image_id)
-print('instance_id:         %s' % instance.instance_id)
-print('instance_type:       %s' % instance.instance_type)
-print('platform:            %s' % instance.platform)
-print('private_dns_name:    %s' % instance.private_dns_name)
-print('private_ip_address:  %s' % instance.private_ip_address)
-print('public_dns_name:     %s' % instance.public_dns_name)
-print('public_ip_address:   %s' % instance.public_ip_address)
-print('security_groups:     %s' % instance.security_groups)
-print('state:               %s' % instance.state)
-print('state_reason:        %s' % instance.state_reason)
-print('vpc:                 %s' % instance.vpc)
-print('subnet:              %s' % instance.subnet)
+if __name__ == '__main__':
+    cli()
